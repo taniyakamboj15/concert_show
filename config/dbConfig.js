4ab6dd0a27099a430f31c819f8124c00e85a6bd4
Taniya Kamboj <taniyakamboj184@gmail.com>
2025-05-22 10:35:38 +0530

    final commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://abhinav:yP2i3MrJ9vZjB8Ff@cluster0.en1oo.mongodb.net/concert?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://taniyakamboj184:taniyaKamboj184@cluster0.sncqn.mongodb.net/event?retryWrites=true&w=majority&appName=Cluster0"
     );
   } catch (err) {
     console.log(err.message);
